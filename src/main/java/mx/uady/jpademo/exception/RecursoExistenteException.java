756fb977b454f6dafb150af3d93867e457d5eb9b
Joaquin Martinez <joaquinmartinez2509@gmail.com>
2019-02-24 13:06:18 -0600

    agrega serialVersionUID para clases serializables

--- src/main/java/mx/uady/jpademo/exception/RecursoExistenteException.java
+++ src/main/java/mx/uady/jpademo/exception/RecursoExistenteException.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class RecursoExistenteException extends RuntimeException{
+	
+	private static final long serialVersionUID = -5672720626654386590L;
 
 	public RecursoExistenteException(String recurso) {
 		super("El recurso " + recurso + " ya existe en el sistema.");
--- src/main/java/mx/uady/jpademo/exception/RecursoNoEncontradoException.java
+++ src/main/java/mx/uady/jpademo/exception/RecursoNoEncontradoException.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class RecursoNoEncontradoException extends RuntimeException{
 
-  public RecursoNoEncontradoException(String recurso) {
+	private static final long serialVersionUID = -6935809960328059065L;
+
+	public RecursoNoEncontradoException(String recurso) {
 		super("El recurso "+ recurso +" no se encuentra en el sistema.");
 	}
 
